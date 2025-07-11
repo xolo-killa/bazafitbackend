3d8a55587f25537af141ad40e9f184272e058d20
xolo-killa <134775308+xolo-killa@users.noreply.github.com>
2025-07-12 00:13:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,6 @@ def save_profile():
     return jsonify({"status": "ok"})
 
 
-# if __name__ == '__main__':
-#     init_db()
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
